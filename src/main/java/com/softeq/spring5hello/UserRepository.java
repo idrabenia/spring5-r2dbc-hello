3a85dd2962884d65286a4c0f77111ca3178b6040
Bitbucket Pipeline <admin@etherisk.com>
2020-05-15 13:39:49 +0300

    Move to Spring Data Reactive M3 version

--- src/main/java/com/softeq/spring5hello/UserRepository.java
+++ src/main/java/com/softeq/spring5hello/UserRepository.java
@@ -1,6 +1,6 @@
 package com.softeq.spring5hello;
 
-import org.springframework.data.r2dbc.repository.query.Query;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
